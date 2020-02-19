074c30b9226649b494c17c8d94f39aad04ce6240
anirudh_r_hub <anirudhramani16@gmail.com>
2020-02-19 18:29:59 +0530

    Added timed activity

--- app/src/main/java/com/example/dynamic_tabs/Survelliance.java
+++ app/src/main/java/com/example/dynamic_tabs/Survelliance.java
@@ -2,15 +2,23 @@ package com.example.dynamic_tabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.provider.AlarmClock;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class Survelliance extends AppCompatActivity {
@@ -18,6 +26,8 @@ public class Survelliance extends AppCompatActivity {
     WebView videoView;
     ImageButton imageButton;
     boolean isPlaying;
+    private AlarmManager alarmMgr;
+    private PendingIntent alarmIntent;
 
     String videoUrl = "http://www.youtube.com/watch?v=ZuG5VuWGad8";
 
@@ -25,10 +35,24 @@ public class Survelliance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survelliance);
-
-       videoView = (WebView) findViewById(R.id.videoStream);
+        /*Button set_alarm=(Button)findViewById(R.id.set_alarm);
+       /*videoView = (WebView) findViewById(R.id.videoStream);
        videoView.setWebViewClient(new WebViewClient());
-       videoView.loadUrl(videoUrl);
+       videoView.loadUrl(videoUrl);*/
+        /*alarmMgr=(AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        //Intent intent=new Intent(this,AlarmReceiver.class);
+        //alarmIntent=PendingIntent.getBroadcast(Survelliance.this,0,intent,0);
+       set_alarm.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+            alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    SystemClock.elapsedRealtime()+AlarmManager.INTERVAL_FIFTEEN_MINUTES,
+                    AlarmManager.INTERVAL_FIFTEEN_MINUTES,alarmIntent);
+            Toast.makeText(getApplicationContext(),"Alarm set",Toast.LENGTH_LONG).show();
+           }
+       });*/
+
+
        /* imageButton = (ImageButton) findViewById(R.id.btnPlayPause);
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(videoView);
---
+++ app/src/main/java/com/example/dynamic_tabs/TimedActivity.java
@@ -0,0 +1,71 @@
+package com.example.dynamic_tabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class TimedActivity extends AppCompatActivity {
+    TimePicker timePicker;
+    Button setAlarm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timed);
+        timePicker = (TimePicker)findViewById(R.id.timepicker);
+            setAlarm = (Button)findViewById(R.id.setAlarm);
+
+            setAlarm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Calendar calendar = Calendar.getInstance();
+                    if(Build.VERSION.SDK_INT >= 23) {
+                        calendar.set(calendar.get(Calendar.YEAR),
+                                calendar.get(Calendar.MONTH),
+                                calendar.get(Calendar.DAY_OF_MONTH),
+                                timePicker.getHour(),
+                                timePicker.getMinute(),
+                                0);
+                    }
+                    else{
+                        calendar.set(calendar.get(Calendar.YEAR),
+                                calendar.get(Calendar.MONTH),
+                                calendar.get(Calendar.DAY_OF_MONTH),
+                                timePicker.getCurrentHour(),
+                                timePicker.getCurrentMinute(),
+                                0);
+                    }
+
+                    SetAlarm(calendar.getTimeInMillis());
+
+                }
+            });
+    }
+
+    private void SetAlarm(long timeInMillis) {
+            AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+            Intent intent = new Intent(this,PublishMessage.class);
+            //intent.putExtra("topic","random");
+            //intent.putExtra("message","ON");
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this,0,intent,0);
+
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,timeInMillis,AlarmManager.INTERVAL_DAY,pendingIntent);
+
+            Toast.makeText(getApplicationContext(),"Alarm is Set",Toast.LENGTH_SHORT).show();
+        }
+}
+
+
+
